ee64a64738450bb649c9ec32898f148589c6f1d4
JorgePfeifer <j.pfeifer01@ufromail.cl>
2025-10-01 13:33:50 -0300

    Se crea el la bases del backend
    Se descargan las dependencias y datos de Spring Initializer y se mueven a las carpetas del .zip a Backend

---
+++ backend/buses-api/src/main/java/cl/venegas/buses_api/BusesApiApplication.java
@@ -0,0 +1,13 @@
+package cl.venegas.buses_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusesApiApplication.class, args);
+	}
+
+}
